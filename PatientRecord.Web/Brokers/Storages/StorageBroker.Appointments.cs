8eecbe814fd526ae522f02034bcd8e1deae08071
shumtakadiyor@gmail.com <shumtakadiyor@gmail.com>
2023-02-08 23:03:12 +0500

    CODE RUB: Rename

--- PatientRecord.Web/Brokers/Storages/StorageBroker.Appointments.cs
+++ PatientRecord.Web/Brokers/Storages/StorageBroker.Appointments.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using PatientRecord.Web.Models;
+using PatientRecord.Web.Models.Appointments;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
