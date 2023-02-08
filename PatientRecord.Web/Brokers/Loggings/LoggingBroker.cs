ea1456f986ec5efb9c87e6fc78b66bc7c35b8537
shumtakadiyor@gmail.com <shumtakadiyor@gmail.com>
2023-02-08 23:02:36 +0500

    BROKERS: Loggings Added

---
+++ PatientRecord.Web/Brokers/Loggings/LoggingBroker.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace PatientRecord.Web.Brokers.Loggings
+{
+    public class LoggingBroker : ILoggingBroker
+    {
+        private ILogger<LoggingBroker> logger;
+
+        public LoggingBroker(ILogger<LoggingBroker> logger)=>
+            this.logger = logger;
+
+
+        public void LogCritical(Exception exception) =>
+            logger.LogCritical(exception, exception.Message);
+
+        public void LogError(Exception exception)=>
+            logger.LogError(exception, exception.Message);
+    }
+}
