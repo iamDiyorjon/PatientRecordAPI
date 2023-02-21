f1ae8cf8bc534ca8e6a715bb781506d4c98bdad3
Diyor <shumtakadiyor@gmail.com>
2023-02-21 10:28:55 +0500

    Add DateTime Broker

---
+++ PatientRecord.Web/Brokers/DataTimes/DateTimeBroker.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace PatientRecord.Web.Brokers.DataTimes
+{
+    public class DateTimeBroker : IDateTimeBroker
+    {
+        public DateTimeOffset GetCurrentDateTime() =>
+            DateTimeOffset.UtcNow;
+    }
+}
---
+++ PatientRecord.Web/Brokers/DataTimes/IDateTimeBroker.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace PatientRecord.Web.Brokers.DataTimes
+{
+    public interface IDateTimeBroker
+    {
+        DateTimeOffset GetCurrentDateTime();
+    }
+}
