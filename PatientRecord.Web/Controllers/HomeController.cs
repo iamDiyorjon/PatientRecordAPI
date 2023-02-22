291e35e56a66fa17596c75b6dc98d3f332b518e2
Diyor <shumtakadiyor@gmail.com>
2023-02-22 16:38:19 +0500

    Add Controller

---
+++ PatientRecord.Web/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace PatientRecord.Web.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : RESTFulController
+    {
+        [HttpGet]
+        public IActionResult Index() => Ok("Project is running ");
+    }
+}
