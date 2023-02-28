fb190680845daece52549b5fb37cd2e90761dc2e
Diyor <shumtakadiyor@gmail.com>
2023-02-28 13:41:16 +0500

    generateGuid

--- PatientRecord.Web/Models/Patients/Patient.cs
+++ PatientRecord.Web/Models/Patients/Patient.cs
@@ -7,13 +7,13 @@ namespace PatientRecord.Web.Models.Patients;
 
 public class Patient
 {
-    public Guid Id { get; set; }
+    public Guid Id { get; set; } = Guid.NewGuid();
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public int Age { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
-    public string Salt { get; set; }
+    public string Salt { get; set; } = "salt";
     public string PhoneNumber { get; set; }
     public ICollection<Appointment> Appointments { get; set; }
 }
