6e5021fbcc029b06f0eec3003f2c26f7e0e85b57
Diyor <shumtakadiyor@gmail.com>
2023-02-28 13:42:31 +0500

    AuotoMapper Class

---
+++ PatientRecord.Web/Services/DTOs/MappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using PatientRecord.Web.Models.Appointments;
+using PatientRecord.Web.Models.Doctors;
+using PatientRecord.Web.Models.Patients;
+
+namespace PatientRecord.Web.Services.DTOs
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<AppointmentDTO, Appointment>();
+            CreateMap<Appointment, AppointmentDTO>();
+
+            CreateMap<DoctorDTO, Doctor>();
+            CreateMap<Doctor, DoctorDTO>();
+
+            CreateMap<PatientDTO, Patient>();
+            CreateMap<Patient, PatientDTO>();
+        }
+    }
+}
